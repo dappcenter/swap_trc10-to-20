9ab92a0e0cc7e2ba552dabf493031fbc9d5e92d3
pironmind <34869675+pironmind@users.noreply.github.com>
2020-07-04 11:40:42 +0300

    add unpaid pool metric

--- contracts/SwapTrc10to20.sol
+++ contracts/SwapTrc10to20.sol
@@ -21,6 +21,8 @@ contract SwapTrc10to20 is Ownable {
 
     mapping(address => uint) private _trc10balance;
     mapping(address => uint) private _trc20balance;
+    
+    uint private _trc20UnpdPool;
 
     constructor(uint _trc10, address _trc20)
         public
@@ -29,29 +31,37 @@ contract SwapTrc10to20 is Ownable {
         trc10id = _trc10;
         trc20 = ITRC20(_trc20);
     }
-
-    function setExchangeRate(uint value, bool positive) public onlyOwner returns (bool) {
-        rate = value;
-        ratePositive = positive;
-
-        return true;
+    
+    function howMuchToDeposit() public view onlyOwner returns (uint) {
+        uint cBal = getTRC20ContractBalance();
+        uint tPoolBal = trc20UnpaidPool();
+        
+        if (cBal >= tPoolBal) {
+            return 0;
+        }
+        
+        return tPoolBal.sub(cBal);
+    }
+    
+    function trc20UnpaidPool() public view returns (uint) {
+        return uint(_trc20UnpdPool);
     }
 
-    function getTRC10Balance(address who) public view returns (uint){
+    function getTRC10Balance(address who) public view returns (uint) {
         return uint(_trc10balance[who]);
     }
 
-    function getTRC20Balance(address who) public view returns (uint){
+    function getTRC20Balance(address who) public view returns (uint) {
         return uint(_trc20balance[who]);
     }
 
-    function getTRC10ContractBalance() public view returns (uint){
+    function getTRC10ContractBalance() public view returns (uint) {
         trcToken id = trc10id;
 
         return uint(address(this).tokenBalance(id));
     }
 
-    function getTRC20ContractBalance() public view returns (uint){
+    function getTRC20ContractBalance() public view returns (uint) {
         return uint(trc20.balanceOf(address(this)));
     }
 
@@ -68,23 +78,40 @@ contract SwapTrc10to20 is Ownable {
 
     function swap() public returns(bool) {
         uint _balanceBefore = _trc10balance[msg.sender];
-        uint _balanceAfter = _withRate(_balanceBefore);
         _trc10balance[msg.sender] = _trc10balance[msg.sender].sub(_balanceBefore);
+        _trc10balance[msg.sender] = 0;
+        
+        uint _balanceAfter = _withRate(_balanceBefore);
         _trc20balance[msg.sender] = _trc20balance[msg.sender].add(_balanceAfter);
         totalSwapTrc20.add(_balanceAfter);
-        //trc20.approve(msg.sender, _balanceAfter);
+        _trc20UnpdPool.add(_balanceAfter);
 
         return true;
     }
 
-
     function withdrawTrc20() public returns(bool) {
+        require(
+            trc20.balanceOf(address(this)) > _trc20balance[msg.sender] && 
+            trc20.balanceOf(address(this)) > 0, 
+            "Not enought balance on contract."
+        );
+        
         uint _amount = _trc20balance[msg.sender];
         _trc20balance[msg.sender] = 0;
         
-        require(_amount > 0, "Zero balance");
+        require(_amount > 0, "User has withdraw zero balance.");
+        
+        _trc20balance[msg.sender] = 0;
+        _trc20UnpdPool.sub(_amount);
 
         return trc20.transfer(msg.sender, _amount);
+    }
+    
+    function setExchangeRate(uint value, bool positive) public onlyOwner returns (bool) {
+        rate = value;
+        ratePositive = positive;
+
+        return true;
     }
     
     /*
@@ -102,15 +129,11 @@ contract SwapTrc10to20 is Ownable {
     }
         
     /*
-     * @dev Withdraw any ERC20 token to owner, except basic ERC20 token
+     * @dev Withdraw any ERC20 token to owner
      */ 
-    function WDRL_Erc20ToOwner(ITRC20 token) public onlyOwner returns (bool) {
-        if (address(token) == address(trc20)) {
-            revert('Owner cannot withdraw the basic erc20 contract.');
-        }
+    function WDRL_AnyErc20ToOwner(ITRC20 token) public onlyOwner returns (bool) {
         return token.transfer(address(uint160(owner())), getTRC20ContractBalance());
     }
-
 
     function _withRate(uint amount) private view returns(uint) {
         if (ratePositive) {
