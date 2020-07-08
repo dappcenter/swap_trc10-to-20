5ec1b307642b31a4db7cabfef408b622e3c1e3e4
gfijrb <gfijr@i.ua>
2020-07-08 18:22:16 +0300

    fix output count balance err

--- contracts/SwapTrc10to20.sol
+++ contracts/SwapTrc10to20.sol
@@ -15,7 +15,10 @@ contract SwapTrc10to20 is Ownable {
     trcToken public trc10id;
     ITRC20 public trc20;
 
-    uint public rate = 100;
+    uint8 trc10decimal;
+    uint8 trc20decimal;
+
+    uint public rate = 1;
     bool public ratePositive = false;
     uint public totalSwapTrc20;
 
@@ -24,12 +27,19 @@ contract SwapTrc10to20 is Ownable {
     
     uint private _trc20UnpdPool;
 
-    constructor(uint _trc10, address _trc20)
+    constructor(
+        uint _trc10,
+        address _trc20,
+        uint8 _trc10dec,
+        uint8 _trc20dec
+    )
         public
         Ownable(msg.sender)
     {
         trc10id = _trc10;
         trc20 = ITRC20(_trc20);
+        trc10decimal = _trc10dec;
+        trc20decimal = _trc20dec;
     }
     
     function howMuchToDeposit() public view onlyOwner returns (uint) {
@@ -80,9 +90,17 @@ contract SwapTrc10to20 is Ownable {
         uint _balanceBefore = _trc10balance[msg.sender];
         _trc10balance[msg.sender] = _trc10balance[msg.sender].sub(_balanceBefore);
         _trc10balance[msg.sender] = 0;
-        
+
+        _balanceBefore = _balanceBefore.mul(10**6);
+
         uint _balanceAfter = _withRate(_balanceBefore);
+        _balanceAfter = _balanceAfter.mul(10**uint(trc20decimal));
+        _balanceAfter = _balanceAfter.div(10**uint(trc10decimal));
+
+        _balanceAfter = _balanceAfter.div(10**6);
+
         _trc20balance[msg.sender] = _trc20balance[msg.sender].add(_balanceAfter);
+
         totalSwapTrc20.add(_balanceAfter);
         _trc20UnpdPool.add(_balanceAfter);
 
@@ -91,9 +109,9 @@ contract SwapTrc10to20 is Ownable {
 
     function withdrawTrc20() public returns(bool) {
         require(
-            trc20.balanceOf(address(this)) > _trc20balance[msg.sender] && 
+            trc20.balanceOf(address(this)) >= _trc20balance[msg.sender] &&
             trc20.balanceOf(address(this)) > 0, 
-            "Not enought balance on contract."
+            "Not enough balance on contract."
         );
         
         uint _amount = _trc20balance[msg.sender];
@@ -102,7 +120,7 @@ contract SwapTrc10to20 is Ownable {
         require(_amount > 0, "User has withdraw zero balance.");
         
         _trc20balance[msg.sender] = 0;
-        _trc20UnpdPool.sub(_amount);
+        _trc20UnpdPool.sub(_amount, "Not enough trc20 balance on contract.");
 
         return trc20.transfer(msg.sender, _amount);
     }
@@ -115,23 +133,23 @@ contract SwapTrc10to20 is Ownable {
     }
     
     /*
-     * @dev Withdraw basic ERC10 token to owner
+     * @dev Withdraw basic TRC10 token to owner
      */ 
-    function WDRL_Erc10ToOwner() public onlyOwner {
+    function WDRL_Trc10ToOwner() public onlyOwner {
         return address(uint160(owner())).transferToken(getTRC10ContractBalance(), trc10id);
     }
     
     /*
-     * @dev Withdraw any ERC10 token to owner
+     * @dev Withdraw any TRC10 token to owner
      */ 
-    function WDRL_AnyErc10AnyToOwner(trcToken id) public onlyOwner {
+    function WDRL_AnyTrc10AnyToOwner(trcToken id) public onlyOwner {
         return address(uint160(owner())).transferToken(getTRC10ContractBalance(), id);
     }
         
     /*
      * @dev Withdraw any ERC20 token to owner
      */ 
-    function WDRL_AnyErc20ToOwner(ITRC20 token) public onlyOwner returns (bool) {
+    function WDRL_AnyTrc20ToOwner(ITRC20 token) public onlyOwner returns (bool) {
         return token.transfer(address(uint160(owner())), getTRC20ContractBalance());
     }
 
